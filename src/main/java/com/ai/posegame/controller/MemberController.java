995b62f96388cb5fc8ebd4fa9abd2c2d05a0cc78
minju <mj00126@gmail.com>
2023-06-21 18:00:01 +0900

    security 프론트 적용 중

--- src/main/java/com/ai/posegame/controller/MemberController.java
+++ src/main/java/com/ai/posegame/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.ai.posegame.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
